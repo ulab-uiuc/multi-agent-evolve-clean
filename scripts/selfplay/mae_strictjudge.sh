21e9fb1a32690791c9d07310155c97d7b0a42ff4
yidingw <blancokdb@gmail.com>
2025-09-15 11:26:26 +0800

    Add include_reference half
    Set the variable include_references as a float probability.

--- scripts/selfplay/mae_strictjudge.sh
+++ scripts/selfplay/mae_strictjudge.sh
@@ -6,7 +6,7 @@ export VLLM_ATTENTION_BACKEND=FLASH_ATTN
 export RAY_memory_monitor_refresh_ms=0
 export RAY_LOGGING_LEVEL=DEBUG
 export HYDRA_FULL_ERROR=1
-export CUDA_VISIBLE_DEVICES="1,2,3,4"
+export CUDA_VISIBLE_DEVICES="5,6,7,8"
 export NCCL_P2P_DISABLE=1
 
 python -m absolute_zero_reasoner.main_azr_ppo \
@@ -46,7 +46,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='general_io_reasoning' \
-    trainer.experiment_name='general_io_3b_withref_16-8bs_valfirst_n1_self_judge_seperate_withanswergeneration_evolveprompt_trainjudge_rejectbadquestion' \
+    trainer.experiment_name='general_io_3b_half_reference_16-8bs_valfirst_n1_self_judge_seperate_withanswergeneration_trainjudge_rejectbadquestion' \
     trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.save_freq=25 \
@@ -74,7 +74,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     azr.train_propose=True \
     azr.reward.n_samples=3 \
     azr.reward.generation_reward_config.format_reward=false \
-    azr.reward.generation_reward_config.include_references=true \
+    azr.reward.generation_reward_config.include_references=0.5 \
     azr.reward.generation_reward_config.generation_accuracy_convertion=inverse \
     azr.reward.generation_reward_config.answer_diversity_reward.hierarchical=false \
     azr.data_selection_strategy.content_max_length=8096 \
@@ -85,7 +85,7 @@ python -m absolute_zero_reasoner.main_azr_ppo \
     trainer.resume_mode=disable \
     trainer.total_epochs=30 \
     +prompt_manager.template_file=absolute_zero_reasoner/data_construction/Initial_prompt_templates/strict.json \
-    azr.enable_actor_prompt_optimization=true \
+    azr.enable_actor_prompt_optimization=false \
     azr.prompt_optimization.frequency=1 \
     azr.prompt_optimization.accuracy_threshold=0.3 \
     azr.prompt_optimization.max_improvements_per_step=3 \
